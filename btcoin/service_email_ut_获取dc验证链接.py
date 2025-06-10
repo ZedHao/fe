bec59ee8bf7832021ef9879d0c215d6e41b772fc
huanghao03 <huanghao03@bilibili.com>
2025-06-10 11:27:14 +0800

    refactor(account): 优化账号控制通知逻辑
    - 修改账号控制通知的处理流程，增加对禁言状态变化的判断
    - 优化日志输出，提高代码可读性
    - 调整时间判断逻辑，提高效率

--- btcoin/service_email_ut_获取dc验证链接.py
+++
@@ -1,9 +0,0 @@
-
-import service_email
-import service_email_account
-
-
-if __name__ == '__main__':
-
-    cur_ServiceEmail = service_email.ServiceEmail('wnxpznek@raymanmail.com', 'szgsgukw1138')
-    cur_ServiceEmail._get_discord_link_from_firstmail_with_IMAP()
